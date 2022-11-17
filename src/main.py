925875976a3f5580b9305b548fcb917604baffa8
dvgniele <ktm17@live.it>
2022-11-17 17:55:19 +0100

    fix: better emoji regex

--- src/main.py
+++ src/main.py
@@ -17,23 +17,23 @@ def tokenize(text: str, regex: list = [], call_emoji_by_name: bool = True):
     emoji_by_name = call_emoji_by_name
 
     re_list = [
+            (r":([a-z]*[_][a-z]*)*[-]?([a-z]*[_]?[a-z]*)*:",                    lambda scanner, token:('EMOJI', print_emoji(token))),
             (r"([<|>}3O~0]?[:;8=BxX%#]['\"]?[-^]?[)(03sSxXDcCpoOPELÞþb/*\\#&$><}{\[\]@|])|([cCD><][-^]?['\"]?[:;8=BxX%#])",  
-                                                                    lambda scanner,token:('EMOTICON', token)),
+                                                                                lambda scanner, token:('EMOTICON', token)),
                                                                     
-            (r"[:][a-z_-]+[:]",                                     lambda scanner,token:('EMOJI', print_emoji(token))),
             (r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]" +
                 "|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"+
                 "|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"+
                 "|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",    
-                                                                    lambda scanner,token:('DOMAIN', token)),
+                                                                                lambda scanner, token:('DOMAIN', token)),
 
-            (r"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?",             lambda scanner,token:('NUMBER', token)),
-            (r"=|\+|-|\*|/",                                        lambda scanner,token:('OPERATOR', token)),
-            (r"[A-zÀ-ú]+",                                         lambda scanner,token:('LITERAL', token)),
-            #(r"(?::|;|X|=)(?:-|')?(?:\)|\(|D|P)",                   lambda scanner,token:('EMOTICON', token)),
-            (r"[.][.][.]",                                          lambda scanner,token:('PUNCTUATION', token)),
-            (r"[.!?,:;/'‘‘’`\"]",                                   lambda scanner,token:('PUNCTUATION', token)),
-            (r"\s+",                                                None), # None == skip token.
+            (r"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?",                         lambda scanner, token:('NUMBER', token)),
+            (r"=|\+|-|\*|/",                                                    lambda scanner, token:('OPERATOR', token)),
+            (r"([u][n]|[l]|[g][l]|[n]|[c]|[m]|[t]|[v]|[s]|[d])[']",             lambda scanner, token:('LITERAL CONTRAPTION', token)),
+            (r"[A-zÀ-ú]+",                                                      lambda scanner, token:('LITERAL', token)),
+            (r"[.][.][.]",                                                      lambda scanner, token:('PUNCTUATION', token)),
+            (r"[.!?,:;/'‘‘’`\"]",                                               lambda scanner, token:('PUNCTUATION', token)),
+            (r"\s+",                                                            None), # None == skip token.
         ]
 
     scanner = re.Scanner(regex + re_list)
