ec5c1fd4be768d4b847b177e76f01b67732534cc
dvgniele <ktm17@live.it>
2022-11-22 12:30:48 +0100

    wip: read from json

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import re
 import surrogates 
 import emoji
+import json
 
 emoji_by_name = True
 
@@ -18,21 +19,21 @@ def tokenize(text: str, regex: list = [], regex_start_pos: int = 0, call_emoji_b
 
     regex_list = [
             (r":([a-z]*[_][a-z]*)*[-]?([a-z]*[_]?[a-z]*)*:",                    lambda scanner, token:('EMOJI', print_emoji(token))),
-            (r"([<|>}3O~0]?[:;8=BxX%#]['\"]?[-^]?[)(03sSxXDcCpoOPELÞþb/*\\#&$><}{\[\]@|])|([cCD><][-^]?['\"]?[:;8=BxX%#])",  
-                                                                                lambda scanner, token:('EMOTICON', token)),
+            # (r"([<|>}3O~0]?[:;8=BxX%#]['\"]?[-^]?[)(03sSxXDcCpoOPELÞþb/*\\#&$><}{\[\]@|])|([cCD><][-^]?['\"]?[:;8=BxX%#])",  
+            #                                                                     lambda scanner, token:('EMOTICON', token)),
                                                                     
-            (r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]" +
-                "|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"+
-                "|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"+
-                "|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",    
-                                                                                lambda scanner, token:('DOMAIN', token)),
+            # (r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]" +
+            #     "|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"+
+            #     "|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"+
+            #     "|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",    
+            #                                                                     lambda scanner, token:('DOMAIN', token)),
 
-            (r"[-+]?[0-9]*(\.|\,)?[0-9]+([eE][-+]?[0-9]+)?(\%|\‰)?",            lambda scanner, token:('NUMBER', token)),
-            (r"=|\+|-|\*|/\%\‰",                                                lambda scanner, token:('OPERATOR', token)),
-            (r"([u][n]|[l]|[g][l]|[n]|[c]|[m]|[t]|[v]|[s]|[d]|[a][l][l])[']",   lambda scanner, token:('LITERAL CONTRACTION', token)),
-            (r"[.][.][.]",                                                      lambda scanner, token:('PUNCTUATION', token)),
-            (r"[.!?,:;/'‘‘’ˈː– `\"\(\)\[\]\{\}]",                               lambda scanner, token:('PUNCTUATION', token)),
-            (r"[A-zÀ-ú]+",                                                      lambda scanner, token:('LITERAL', token)),
+            # (r"[-+]?[0-9]*(\.|\,)?[0-9]+([eE][-+]?[0-9]+)?(\%|\‰)?",            lambda scanner, token:('NUMBER', token)),
+            # (r"=|\+|-|\*|/\%\‰",                                                lambda scanner, token:('OPERATOR', token)),
+            # (r"([u][n]|[l]|[g][l]|[n]|[c]|[m]|[t]|[v]|[s]|[d]|[a][l][l])[']",   lambda scanner, token:('LITERAL CONTRACTION', token)),
+            # (r"[.][.][.]",                                                      lambda scanner, token:('PUNCTUATION', token)),
+            # (r"[.!?,:;/'‘‘’ˈː– `\"\(\)\[\]\{\}]",                               lambda scanner, token:('PUNCTUATION', token)),
+            # (r"[A-zÀ-ú]+",                                                      lambda scanner, token:('LITERAL', token)),
             (r"\s+",                                                            None), # None == skip token.
         ]
 
@@ -43,7 +44,9 @@ def tokenize(text: str, regex: list = [], regex_start_pos: int = 0, call_emoji_b
         for index, item in enumerate(regex):
             regex_list.insert(index, item)
     
-    scanner = re.Scanner(regex_list)
+    scanner = re.Scanner
+    (regex_list)
+    print(scanner)
 
     demojized_text = emoji.demojize(text) 
     results, unknown_chars = scanner.scan(demojized_text)
@@ -55,14 +58,29 @@ def log(text):
     print('\n' + separator + text + separator + '\n')
 
 def main():
+    config_file = open('data/config/formatted.json')
+    config_data = json.load(config_file)
+    config_file.close()
+
+    custom_regex_lst = []
+    for item in config_data['config']:
+        print(item)
+        custom_regex_lst.append(
+            (r"{}".format(item['regex']), lambda scanner,token:(item['name'], token))
+        )
+
+
+    print(custom_regex_lst)
+
     log('TOKENIZER')
 
     text = read('data/input/input.txt')
 
-    custom_regex_lst = [
-        #(r"[.][.][.]", lambda scanner,token:('TESTONE', token))
-    ]
-    tokenized, skipped = tokenize(text = text, regex = custom_regex_lst, regex_start_pos=0)
+    # custom_regex_lst = [
+    #     #(r"[.][.][.]", lambda scanner,token:('TESTONE', token))
+    # ]
+
+    tokenized, skipped = tokenize(text = text, regex = custom_regex_lst, regex_start_pos=1)
 
     log('TOKENIZED  ')
     print('text:\t' + text + '\n')
